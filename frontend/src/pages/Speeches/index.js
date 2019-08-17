66ec3582dd7dc8eb9bcd78d9054f15f4bad727a5
Márcio Silva <marcio.mcbs@gmail.com>
2019-08-16 23:23:10 -0300

    Atualização do código de ciclo de vida > useEffect

--- frontend/src/pages/Speeches/index.js
+++ frontend/src/pages/Speeches/index.js
@@ -1,140 +1,138 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header';
 import './index.css';
 
 const api = require('../../services/api');
 
-export default class ListaDiscursos extends Component {
-  state = {
-    discursos: [],
-    startedSpeech: {},
-    buttonStartDisabled: false,
-    seconds: 0,
-    textCronometer: "00:00",
-    classFooter: "footer-white",
-    running: false,
-    socket: null,
-  };
+export default function Speeches() {
+    const [ speeches, setSpeeches ] = useState([]);
+    const [ textCronometer, setTextCronometer ] = useState("00:00");
+    const [ running, setRunnning ] = useState(false);
+    const [ seconds, setSeconds ] = useState(0);
+    const [ startedSpeech, setStartedSpeech ] = useState({});
+    const [ classFooter, setClassFooter ] = useState("footer-white");
 
-  colorVerify = () => {
-    if(((this.state.startedSpeech.speech.time * 60) - this.state.seconds) <= 60){
-      if(((this.state.startedSpeech.speech.time * 60) - this.state.seconds) < 0) this.setState({classFooter: "footer-red"});
+    const getSpeeches = async () => {
+        const { data } = await api.get("/speeches");
+        const { speeches, lastCommand } = data;
+        if(lastCommand.method === 'start'){
+            console.log(lastCommand);
+            setStartedSpeech(lastCommand);
+            setSeconds(Math.floor(new Date().getTime() / 1000) - Math.floor(new Date(lastCommand.speech.startedAt).getTime() / 1000));
+            setRunnning(true);
+        }
+        setSpeeches(speeches);
+    }
+
+    useEffect(() => {
+        getSpeeches();
+    }, []);
+
+    useEffect(() => {
+        if(running === false) {
+            setSeconds(0);
+            setStartedSpeech({});
+            setClassFooter("footer-white");
+            setTextCronometer("00:00");
+        }
+    }, [ running, seconds ]);
+
+    useEffect(() => {
+        console.log(running, seconds);
+        if(running === true){
+            setTimeout(() => {
+                setSeconds(seconds + 1);
+                setTextCronometer(`${formatMinutes()}:${("0" + (seconds % 60)).slice(-2)}`);
+                colorVerify();
+            }, 1000);
+        }
+    // eslint-disable-next-line
+    }, [ running, seconds ]);
+
+    const startSpeech = async idSpeech => {
+        const { data } = await api.get(`/speeches/start?id=${idSpeech}`);
+        if(!!data.msg) alert(data.msg);
+        else {
+            setStartedSpeech({method:"start", speech: data});
+            setRunnning(true);
+        };
+    }
+
+    const stopSpeech = async () => {
+      const { data } = await api.get(`/speeches/stop`);
+      if(!!data.msg) alert(data.msg);
       else {
-        if((this.state.seconds % 2) === 0) this.setState({classFooter: "footer-red"});
-        else this.setState({classFooter: "footer-black"});
-      }
-    }
-  }
-
-  formatMinutes = () => {
-    const minutes = Math.floor(this.state.seconds / 60);
-    if(minutes.toString().length === 1) return `0${minutes}`;
-    else return minutes;
-  }
-
-  runClocker = () => {
-    this.formatMinutes();
-    setInterval(() => {
-      if(this.state.running){
-          this.setState({seconds: this.state.seconds + 1});
-          this.setState({textCronometer: `${this.formatMinutes()}:${("0" + (this.state.seconds % 60)).slice(-2)}`});
-          this.colorVerify();
-      }
-    }, 1000);
-  };
-
-  componentDidMount = async e => {
-    this.runClocker();
-
-    const { data } = await api.get('/speeches');
-    const { speeches, lastCommand } = data;
-
-    this.setState({discursos: speeches});
-
-    if(lastCommand.method === "start"){
-      this.setState({buttonStartDisabled: true});
-      this.setState({startedSpeech: lastCommand});
-      this.setState({running: true});
-      this.setState({seconds: (Math.floor(new Date().getTime() / 1000) - Math.floor(new Date(lastCommand.speech.startedAt).getTime() / 1000))});
-    }
-  };
-
-  startSpeech = async idSpeech => {
-    const { data } = await api.get(`/speeches/start?id=${idSpeech}`);
-    if(!!data.msg) alert(data.msg);
-    else {
-      this.setState({buttonStartDisabled: true});
-      this.setState({startedSpeech: {method:"start", speech: data}});
-      this.setState({running: true});
+        setRunnning(false);
+        setSeconds(0);
+        setStartedSpeech({});
+        setClassFooter("footer-white");
+        setTextCronometer("00:00");
+      };
     };
-  };
 
-  stopSpeech = async () => {
-    const { data } = await api.get(`/speeches/stop`);
-    if(!!data.msg) alert(data.msg);
-    else {
-      this.setState({buttonStartDisabled: false});
-      this.setState({startedSpeech: {}});
-      this.setState({seconds: 0});
-      this.setState({textCronometer: "00:00"});
-      this.setState({classFooter: "footer-white"});
-      this.setState({running: false});
-    };
-  };
+    const formatMinutes = () => {
+      const minutes = Math.floor(seconds / 60);
+      if(minutes.toString().length === 1) return `0${minutes}`;
+      else return minutes;
+    }
 
-  render() {
-
-    const discursosTr = this.state.discursos.map( discurso => {
-        return (
-            <tr key={discurso.id}>
-                <td>{ discurso.title }</td>
-                <td>{ discurso.time }</td>
-                <td>
-                  <button 
-                    className="startSpeech" 
-                    onClick={ () => this.startSpeech(discurso.id) } 
-                    disabled={ this.state.buttonStartDisabled }
-                  > Iniciar </button>
-                </td>
-            </tr>
-        );
-    });
+    const colorVerify = () => {
+        if(((startedSpeech.speech.time * 60) - seconds) <= 60){
+            if(((startedSpeech.speech.time * 60) - seconds) < 0) setClassFooter("footer-red");
+            else {
+                if((seconds % 2) === 0) setClassFooter("footer-red");
+                else setClassFooter("footer-black");
+            }
+        }
+    }
 
     return (
-      <div>
-        <Header />
-        <div className="container">
-          <div className="list-speeches-container">
-            <table>
-                <thead>
-                  <tr>
-                    <th>Título</th>
-                    <th>Tempo (Minutos)</th>
-                    <th>Ação</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    { discursosTr }
-                </tbody>
-            </table>
-          </div>
-
-          <footer className={this.state.classFooter}>
-            <div className="speech-details">
-
+        <div>
+          <Header />
+          <div className="container">
+            <div className="list-speeches-container">
+              <table>
+                  <thead>
+                    <tr>
+                      <th>Título</th>
+                      <th>Tempo (Minutos)</th>
+                      <th>Ação</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      {
+                        speeches.map(speech => (
+                            <tr key={ speech.id }>
+                                <td>{ speech.title }</td>
+                                <td>{ speech.time }</td>
+                                <td>
+                                    <button 
+                                        className="startSpeech" 
+                                        onClick={ () => startSpeech(speech.id) } 
+                                        disabled={ running }
+                                    > Iniciar </button>
+                                </td>
+                            </tr>
+                        ))
+                      }
+                  </tbody>
+              </table>
             </div>
-            <div className="clock-view">
-              { this.state.textCronometer }
-              <div className="clock-stop" onClick={ () => this.stopSpeech() }>
-
+  
+            <footer className={classFooter}>
+              <div className="speech-details">
+  
               </div>
-            </div>
-            <div className="clock-functions">
-
-            </div>
-          </footer>
+              <div className="clock-view">
+                { textCronometer }
+                <div className="clock-stop" onClick={() => stopSpeech()}>
+                </div>
+              </div>
+              <div className="clock-functions">
+  
+              </div>
+            </footer>
+          </div>
         </div>
-      </div>
     );
-  }
 }
